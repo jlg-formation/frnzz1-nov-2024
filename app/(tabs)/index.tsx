432b9d7e7cb15ccd28e64675dc15d16c62cc0e40
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-11-26 14:01:58 +0100

    dev: added another state

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,9 +7,11 @@ import { StyleSheet, View } from "react-native";
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  console.log("je joue index");
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined
   );
+  const [showAppOptions, setShowAppOptions] = useState(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -20,6 +22,7 @@ export default function Index() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert("You did not select any image.");
     }
@@ -33,14 +36,21 @@ export default function Index() {
           selectedImage={selectedImage}
         />
       </View>
-      <View style={styles.footerContainer}>
-        <Button
-          theme="primary"
-          label="Choose a photo"
-          onPress={pickImageAsync}
-        />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
     </View>
   );
 }
