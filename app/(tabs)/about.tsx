712d65a7935920e04ab6cc31239cca4a33055b3e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-11-27 11:51:12 +0100

    dev: about wiht button

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,25 @@
-import { Text, View, StyleSheet } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useState } from "react";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 
 export default function AboutScreen() {
+  const [message, setMessage] = useState("");
+
+  const onPress = () => {
+    console.log("coucou");
+    if (message !== "") {
+      setMessage("");
+      return;
+    }
+    setMessage("Coucou !!!");
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Pressable style={styles.button} onPress={onPress}>
+        <FontAwesome name="heart" size={18} style={styles.buttonIcon} />
+        <Text style={styles.buttonLabel}>Clique moi</Text>
+      </Pressable>
+      <Text style={styles.message}>{message}</Text>
     </View>
   );
 }
@@ -14,8 +30,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     justifyContent: "center",
     alignItems: "center",
+    gap: 10,
   },
   text: {
     color: "#fff",
+  },
+  button: {
+    borderRadius: 9,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "white",
+    flexDirection: "row",
+    gap: 10,
+  },
+  buttonIcon: {
+    color: "white",
+  },
+  buttonLabel: {
+    color: "white",
+  },
+  message: {
+    height: 20,
+    color: "white",
   },
 });
